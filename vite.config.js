77f27c35d461c190dd11ff55a3b19429fc98c440
pisitsusuk <pisitsusuk@gmail.com>
2025-09-07 01:38:41 +0700

    เปลี่ยนพอตจ้า

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    port: process.env.PORT || 5173, // ใช้พอร์ตที่ Render กำหนด หรือพอร์ตเริ่มต้น 5173
+    host: '0.0.0.0', // ทำให้แอปสามารถเข้าถึงจากเครือข่ายภายนอกได้
+  },
+});
